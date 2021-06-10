a0df39d4e827b4482157f38ce558f487afe0412a
Zuzanna Jasińska <zuzanna.jasinska07@gmail.com>
2021-06-10 13:49:29 +0200

    added auto refresh

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,19 @@
-/*********
-  Rui Santos
-  Complete project details at http://randomnerdtutorials.com  
-*********/
-
 // Load Wi-Fi library
 #include <ESP8266WiFi.h>
+
 
 const double VCC = 3.3;             // NodeMCU on board 3.3v vcc
 const double R2 = 10000;            // 10k ohm series resistor
 const double adc_resolution = 1023; // 10-bit adc
 
-const double A = 0.001129148; // thermistor equation parameters
+const double A = 0.001129148;   // thermistor equation parameters
 const double B = 0.000234125;
-const double C = 0.0000000876741;
+const double C = 0.0000000876741; 
+
 
 // Replace with your network credentials
-const char *ssid = "TUTAJ NAZWA WIFI";
-const char *password = "TUTAJ HASŁO WIFI";
+const char* ssid     = "Galaxy M51EC87";
+const char* password = "wxmy8436";
 
 // Set web server port number to 80
 WiFiServer server(80);
@@ -24,7 +21,7 @@ WiFiServer server(80);
 // Variable to store the HTTP request
 String header;
 
-// Auxiliary variables to store the current output state
+// Auxiliar variables to store the current output state
 String output5State = "off";
 String output4State = "off";
 
@@ -35,12 +32,11 @@ const int output4 = 4;
 // Current time
 unsigned long currentTime = millis();
 // Previous time
-unsigned long previousTime = 0;
+unsigned long previousTime = 0; 
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
 
-void setup()
-{
+void setup() {
   Serial.begin(115200); // define baud rate for serial communication
   // Initialize the output variables as outputs
   pinMode(output5, OUTPUT);
@@ -53,8 +49,7 @@ void setup()
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
@@ -66,140 +61,118 @@ void setup()
   server.begin();
 }
 
-float temperature()
-{
-  double Volt, Rth, temperature, adc_value;
+float temperature(){
+  double Vout, Rth, temperature, adc_value; 
 
-  adc_value = analogRead(A0);                // get measurement
-  Volt = (adc_value * VCC) / adc_resolution; // calculate measurement to volt
-  Rth = (VCC * R2 / Volt) - R2;              // calculate volt to resistance
+  adc_value = analogRead(A0);
+  Vout = (adc_value * VCC) / adc_resolution;
+  Rth = (VCC * R2 / Vout) - R2;
 
-  /*  Steinhart-Hart Thermistor Equation:
+/*  Steinhart-Hart Thermistor Equation:
  *  Temperature in Kelvin = 1 / (A + B[ln(R)] + C[ln(R)]^3)
  *  where A = 0.001129148, B = 0.000234125 and C = 8.76741*10^-8  */
-  temperature = (1 / (A + (B * log(Rth)) + (C * pow((log(Rth)), 3)))); // Temperature in kelvin
+  temperature = (1 / (A + (B * log(Rth)) + (C * pow((log(Rth)),3))));   // Temperature in kelvin
 
-  temperature = temperature - 273.15; // Temperature in degree celsius
-
+  temperature = temperature - 273.15;  // Temperature in degree celsius
+  
   Serial.print("Temperature = ");
   Serial.print(temperature);
   Serial.println(" degree celsius");
   return temperature;
-  //  delay(500);
+//  delay(500);
 }
 
-void loop()
-{
-  WiFiClient client = server.available(); // Listen for incoming clients
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
 
-  if (client)
-  {                                // If a new client connects,
-    Serial.println("New Client."); // print a message out in the serial port
-    String currentLine = "";       // make a String to hold incoming data from the client
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
     currentTime = millis();
     previousTime = currentTime;
-    while (client.connected() && currentTime - previousTime <= timeoutTime)
-    { // loop while the client's connected
-      currentTime = millis();
-      if (client.available())
-      {                         // if there's bytes to read from the client,
-        char c = client.read(); // read a byte, then
-        Serial.write(c);        // print it out the serial monitor
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
+      currentTime = millis();         
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
         header += c;
-        if (c == '\n')
-        { // if the byte is a newline character
+        if (c == '\n') {                    // if the byte is a newline character
           // if the current line is blank, you got two newline characters in a row.
           // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0)
-          {
+          if (currentLine.length() == 0) {
             // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
             // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
-
+            
             // turns the GPIOs on and off
-            if (header.indexOf("GET /5/on") >= 0)
-            {
+            if (header.indexOf("GET /5/on") >= 0) {
               Serial.println("GPIO 5 on");
               output5State = "on";
               digitalWrite(output5, HIGH);
-            }
-            else if (header.indexOf("GET /5/off") >= 0)
-            {
+            } else if (header.indexOf("GET /5/off") >= 0) {
               Serial.println("GPIO 5 off");
               output5State = "off";
               digitalWrite(output5, LOW);
-            }
-            else if (header.indexOf("GET /4/on") >= 0)
-            {
+            } else if (header.indexOf("GET /4/on") >= 0) {
               Serial.println("GPIO 4 on");
               output4State = "on";
               digitalWrite(output4, HIGH);
-            }
-            else if (header.indexOf("GET /4/off") >= 0)
-            {
+            } else if (header.indexOf("GET /4/off") >= 0) {
               Serial.println("GPIO 4 off");
               output4State = "off";
               digitalWrite(output4, LOW);
             }
-
+            
             // Display the HTML web page
             client.println("<!DOCTYPE html><html>");
             client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<meta http-equiv=\"refresh\" content=\"3\">");
             client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons
-
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
             client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
             client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
             client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
             client.println(".button2 {background-color: #77878A;}</style></head>");
-
+            
             // Web Page Heading
             client.println("<body><h1>ESP8266 Web Server</h1>");
-
-            // Display current state, and ON/OFF buttons for LED
+            
+            // Display current state, and ON/OFF buttons for LED  
             client.println("<p>LED - State " + output5State + "</p>");
-            // If the output5State is off, it displays the ON button
-            if (output5State == "off")
-            {
+            // If the output5State is off, it displays the ON button       
+            if (output5State=="off") {
               client.println("<p><a href=\"/LED/on\"><button class=\"button\">ON</button></a></p>");
-            }
-            else
-            {
+            } else {
               client.println("<p><a href=\"/LED/off\"><button class=\"button button2\">OFF</button></a></p>");
-            }
-
+            } 
+            
+               
             // Display current state, and ON/OFF buttons for ESP8266
             client.println("<p>ESP8266 - State " + output4State + "</p>");
-            // If the output4State is off, it displays the ON button
-            if (output4State == "off")
-            {
+            // If the output4State is off, it displays the ON button       
+            if (output4State=="off") {
               client.println("<p><a href=\"/ESP/on\"><button class=\"button\">ON</button></a></p>");
-            }
-            else
-            {
+            } else {
               client.println("<p><a href=\"/ESP/off\"><button class=\"button button2\">OFF</button></a></p>");
             }
             client.println("<p><b>Water temperature</b></p>");
             client.print(temperature());
             client.println("<p><sup>o</sup>C</p>");
             client.println("</body></html>");
-
+            
             // The HTTP response ends with another blank line
             client.println();
             // Break out of the while loop
             break;
-          }
-          else
-          { // if you got a newline, then clear currentLine
+          } else { // if you got a newline, then clear currentLine
             currentLine = "";
           }
-        }
-        else if (c != '\r')
-        {                   // if you got anything else but a carriage return character,
-          currentLine += c; // add it to the end of the currentLine
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
         }
       }
     }
@@ -210,4 +183,5 @@ void loop()
     Serial.println("Client disconnected.");
     Serial.println("");
   }
+
 }
